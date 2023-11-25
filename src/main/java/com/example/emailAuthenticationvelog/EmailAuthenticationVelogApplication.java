6841319403887a8595a5b1d7939f3604577dc1e8
ysh <dbtmdgks7897@outlook.kr>
2023-11-26 08:20:57 +0900

    first commit

---
+++ src/main/java/com/example/emailAuthenticationvelog/EmailAuthenticationVelogApplication.java
@@ -0,0 +1,13 @@
+package com.example.emailAuthenticationvelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailAuthenticationVelogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailAuthenticationVelogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/emailAuthenticationvelog/EmailAuthenticationVelogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emailAuthenticationvelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailAuthenticationVelogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
